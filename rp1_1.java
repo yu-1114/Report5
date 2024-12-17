fc787ea7305410ddcc11b5a367519654fe6e9206
Yu Takeshima <e245704@ie.u-ryukyu.ac.jp>
2024-12-17 19:13:02 +0900

    3rd

--- rp1_1.java
+++ rp1_1.java
@@ -1,13 +1,6 @@
 public class rp1_1 {
     public static void main(String[] args) {
-        String str;
-        str = null;
-
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException が発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
